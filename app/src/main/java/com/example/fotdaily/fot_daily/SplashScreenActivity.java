3165e8a7dc7aa1cf3fcddc59b062052d507bc102
kavinda2024 <kavindanelum.kill@gmail.com>
2025-06-08 02:51:54 +0530

    created_splash screen
    created splash screen and updated the img

---
+++ app/src/main/java/com/example/fotdaily/fot_daily/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.example.fotdaily.fot_daily;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3 seconds delay
+    }
+}
